39530031fbe5aa7e94f94ac99c02b231d4812efc
MaalainineB <bouchalga60@gmail.com>
2023-11-02 14:17:36 +0100

    refactor code in statusMapper

--- src/main/java/com/example/demo/mappers/StatusMapper.java
+++ src/main/java/com/example/demo/mappers/StatusMapper.java
@@ -2,7 +2,6 @@ package com.example.demo.mappers;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.entity.Status;
@@ -10,7 +9,8 @@ import com.example.demo.entity.Status;
 @Mapper
 public interface StatusMapper {
 	@Select("SELECT * FROM status WHERE st_id = #{stId}")
-	@Results({ @Result(property = "stId", column = "st_id"), @Result(property = "stName", column = "st_name"),
-			@Result(property = "stDescription", column = "st_Description") })
+	@Result(property = "stId", column = "st_id")
+	@Result(property = "stName", column = "st_name")
+	@Result(property = "stDescription", column = "st_Description")
 	Status findStatusById(int stId);
 }
