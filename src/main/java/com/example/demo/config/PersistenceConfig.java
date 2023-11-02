bdb1e54218a6b206e056648e007be31735022628
MaalainineB <bouchalga60@gmail.com>
2023-11-02 08:24:56 +0100

    logger added and the configuartion of database modified

--- src/main/java/com/example/demo/config/PersistenceConfig.java
+++ src/main/java/com/example/demo/config/PersistenceConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,6 +14,57 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 @Configuration
 public class PersistenceConfig {
 
+	Logger logger = LoggerFactory.getLogger(PersistenceConfig.class);
+	private String dBType = "postgres";
+
+	// configuration de la BDD embarquée H2
+	@Bean
+	public DataSource dataSource() {
+		if (dBType.contains("H2")) {
+//			// Configuration for H2 DB
+//			return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+			dataSource.setDriverClassName("org.h2.Driver");
+			dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+			dataSource.setUsername("sa");
+			dataSource.setPassword("");
+			return dataSource;
+		} else {
+//			// Configuration for PostgreSQL
+			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+			dataSource.setDriverClassName("org.postgresql.Driver");
+			dataSource.setUrl("jdbc:postgresql://localhost:5432/Cockpit-app");
+			dataSource.setUsername("postgres");
+			dataSource.setPassword("1234");
+			return dataSource;
+		}
+
+	}
+
+	// Initialisation de postgres BDD
+	@Bean
+	public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+
+		if (dBType.contains("H2")) {
+			logger.info("Executing data-test.sql");
+			populator.addScript(new ClassPathResource("data-test.sql"));
+		} else {
+			logger.info("Executing schema.sql");
+			populator.addScript(new ClassPathResource("schema.sql"));
+
+			logger.info("Executing data.sql");
+			populator.addScript(new ClassPathResource("data.sql"));
+		}
+
+		DataSourceInitializer initializer = new DataSourceInitializer();
+		initializer.setDataSource(dataSource);
+		initializer.setDatabasePopulator(populator);
+
+		return initializer;
+	}
+
+	// commented code for Spring profiles
 //	// Configuration for PostgreSQL
 //	@Profile("dev")
 //	@Bean
@@ -30,7 +83,7 @@ public class PersistenceConfig {
 //		initializer.setDatabasePopulator(populator);
 //		return initializer;
 //	}
-//
+
 //	@Profile("test")
 //	@Bean
 //	public DataSourceInitializer dataSourceInitializer() {
@@ -48,43 +101,4 @@ public class PersistenceConfig {
 //		return initializer;
 //	}
 
-//	private final Environment environment;
-//
-//	// Configuration for PostgreSQL
-//	@Profile("dev")
-//	@Bean
-//	public DataSource postgresDataSource() {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName("org.postgresql.Driver");
-//		dataSource.setUrl("jdbc:postgresql://localhost:5432/Cockpit-app");
-//		dataSource.setUsername("postgres");
-//		dataSource.setPassword("1234");
-//		return dataSource;
-//	}
-//
-	@Bean
-	public DataSource dataSource() {
-//		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
-
-	@Bean
-	public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-
-		// Check active profiles and add scripts accordingly
-		System.err.println("Executing data-test.sql");
-		populator.addScript(new ClassPathResource("data-test.sql"));
-
-		DataSourceInitializer initializer = new DataSourceInitializer();
-		initializer.setDataSource(dataSource);
-		initializer.setDatabasePopulator(populator);
-
-		return initializer;
-	}
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS status (
+    st_id SERIAL PRIMARY KEY,
+    st_name VARCHAR(50) NOT NULL,
+    st_description VARCHAR(255) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS subcontractor (
+    s_id SERIAL PRIMARY KEY,
+    s_name VARCHAR(250) NOT NULL,
+    s_email VARCHAR(45) NOT NULL,
+    s_fk_status_id SMALLINT NOT NULL, 
+    FOREIGN KEY (s_fk_status_id) REFERENCES status(st_id)
+);
