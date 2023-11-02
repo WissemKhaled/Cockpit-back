62ddd15b82dd86eb4444ff4624bfdd041ef61686
MaalainineB <bouchalga60@gmail.com>
2023-11-02 14:22:18 +0100

    AlreadyArchivedSubcontractor exception error were corrected

---
+++ src/main/java/com/example/demo/exception/AlreadyArchivedSubcontractor.java
@@ -0,0 +1,9 @@
+package com.example.demo.exception;
+
+public class AlreadyArchivedSubcontractor extends Exception {
+
+	public AlreadyArchivedSubcontractor(String message) {
+		super(message);
+	}
+
+}
