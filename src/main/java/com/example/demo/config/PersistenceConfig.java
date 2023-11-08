9d78f06235d6284e1d0ee310ae6662ef0f0b67ed
Younes BOUAZIZ <YBO24080@open-groupe.com>
2023-11-08 08:56:48 +0100

    Ajouts de commentaires et nettoyage du code

--- src/main/java/com/example/demo/config/PersistenceConfig.java
+++ src/main/java/com/example/demo/config/PersistenceConfig.java
@@ -21,32 +21,38 @@ import lombok.extern.java.Log;
 @Configuration
 @MapperScan("com.example.demo.mappers")
 public class PersistenceConfig {
+	/**
+	 * Afin de faire fonctionner la configuration de 2 base de données (une h2 de test et une postgre de dev), modification du scope dans le pom.xlm :
+	 * pour la dépendance h2 : scope test (bdd utilisée seulement pour les tests
+	 * pour la dépendance postgre : scope compile (bdd utilisée pour la compilation)
+	*/
+	
 	private String dBType = "H2";
 	
 	// configuration de la BDD embarquée H2
-		@Bean
-		public DataSource dataSource() {
-			DriverManagerDataSource dataSource = new DriverManagerDataSource();
-			System.out.println("initialisation base");
-			if (dBType.equals("H2")) {
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		System.out.println("initialisation base");
+		if (dBType.equals("H2")) {
 //				// Configuration for H2 DB
 //				return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
-				dataSource.setDriverClassName("org.h2.Driver");
-				dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-				dataSource.setUsername("sa");
-				dataSource.setPassword("");
-				System.out.println("base H2");
-			} else if (dBType.equals("postgres")) {
+			dataSource.setDriverClassName("org.h2.Driver");
+			dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+			dataSource.setUsername("sa");
+			dataSource.setPassword("");
+			System.out.println("base H2");
+		} else if (dBType.equals("postgres")) {
 //				// Configuration for PostgreSQL
-				dataSource.setDriverClassName("org.postgresql.Driver");
-				dataSource.setUrl("jdbc:postgresql://localhost:5432/Cockpit-app");
-				dataSource.setUsername("postgres");
-				dataSource.setPassword("postgres");
-				System.out.println("initialisation postgres");
-			}
-			return dataSource;
-
+			dataSource.setDriverClassName("org.postgresql.Driver");
+			dataSource.setUrl("jdbc:postgresql://localhost:5432/Cockpit-app");
+			dataSource.setUsername("postgres");
+			dataSource.setPassword("postgres");
+			System.out.println("initialisation postgres");
 		}
+		return dataSource;
+
+	}
 
 		// Initialisation de postgres BDD
 		@Bean
--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -41,40 +41,7 @@ public class UserControllerTest {
     @MockBean
     private RefreshTokenService refreshTokenService;
 
-//    @Value("${jwt.secret}")
-//    private String SECRET;
-//    
-//    @Value("${token.expiration.duration}")
-//	private int tokenExpirationDuration;
-//
-//    // Method to generate a test JWT token
-//    private String generateTestToken() {
-//        String userName = "user@example.com"; // Set the expected username
-//        Date expirationDate = new Date(System.currentTimeMillis() + 1000 * tokenExpirationDuration); // Set the expected expiration date
-//
-//        return Jwts.builder()
-//                .setSubject(userName)
-//                .setIssuedAt(new Date(System.currentTimeMillis()))
-//                .setExpiration(expirationDate)
-//                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
-//                .compact();
-//    }
-//
-//    @Test
-//    public void testFindUserByEmail_WithValidToken_ShouldReturnUserDetails() throws Exception {
-//        UUserDTO userDTO = new UUserDTO(1, "user@example.com", "John", "Doe", true, LocalDateTime.now(), null);
-//        when(service.findUserByEmail("user@example.com")).thenReturn(userDTO);
-//
-//        String validToken = generateTestToken();
-//        
-//        System.out.println(validToken);
-//
-//        mockMvc.perform(get("/auth/user/userdetails")
-//                .header("Authorization", "Bearer " + validToken))
-//                .andExpect(status().isOk());
-//    }
     
-//
     @Test
     public void testAddNewUser_WithValidData_ShouldReturnCreated() throws Exception {
         CreateUserDTO userDTO = new CreateUserDTO(0, "newuser@example.com", "Password1!", "John", "Doe", true, null, null);
@@ -84,37 +51,16 @@ public class UserControllerTest {
                 .content(new ObjectMapper().writeValueAsString(userDTO)))
                 .andExpect(status().isCreated());
     }
-//
-//
-//    @Test
-//    public void testAuthenticateAndGetToken_WithValidCredentials_ShouldReturnStatusOk() throws Exception {
-//        AuthRequest authRequest = new AuthRequest("user@example.com", "Password1!");
-//
-//        // Mock the behavior of the service to return an authenticated user
-//        UUserDTO userDTO = new UUserDTO(1, "user@example.com", "John", "Doe", true, LocalDateTime.now(), LocalDateTime.now());
-//        when(service.findUserByEmail("user@example.com")).thenReturn(userDTO);
-//
-//        mockMvc.perform(post("/auth/generateToken")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(new ObjectMapper().writeValueAsString(authRequest)))
-//                .andExpect(status().isOk());
-//    }
-//
-//
-//    @Test
-//    public void testRefreshToken_WithValidToken_ShouldReturnToken() throws Exception {
-//        RefreshTokenRequestDTO refreshTokenRequestDTO = new RefreshTokenRequestDTO("validRefreshToken");
-//
-//        // Mock the behavior of the refreshTokenService
-//        when(refreshTokenService.findByToken("validRefreshToken")).thenReturn(Optional.of(new RefreshToken()));
-//
-//        // Mock the JWT service to generate a new token
-//        when(jwtService.generateToken(any())).thenReturn("newAccessToken");
-//
-//        mockMvc.perform(post("/auth/refreshToken")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(new ObjectMapper().writeValueAsString(refreshTokenRequestDTO)))
-//                .andExpect(status().isOk());
-//    }
+    
+    @Test
+    public void testAddNewUser_WithInvalidData_ShouldReturnBadRequest() throws Exception {
+        // Create a user with missing email
+        CreateUserDTO userDTO = new CreateUserDTO(0, null, "Password1!", "John", "Doe", true, null, null);
+
+        mockMvc.perform(post("/auth/addNewUser")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userDTO)))
+                .andExpect(status().isBadRequest());
+    }
 
 }
