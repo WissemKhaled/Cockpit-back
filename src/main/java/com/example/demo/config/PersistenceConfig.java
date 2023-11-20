b8081ad165ab88a8dc1032b5e14e3ba5582cbde3
MaalainineB <bouchalga60@gmail.com>
2023-11-20 16:02:39 +0100

    modify sql script to add the service provider in the DB

--- src/main/java/com/example/demo/config/PersistenceConfig.java
+++ src/main/java/com/example/demo/config/PersistenceConfig.java
@@ -36,12 +36,12 @@ public class PersistenceConfig {
 			dataSource.setDriverClassName("org.postgresql.Driver");
 			dataSource.setUrl("jdbc:postgresql://localhost:5432/Cockpit-app");
 			dataSource.setUsername("postgres");
-			dataSource.setPassword("postgres");
+			dataSource.setPassword("1234");
 		}
 		return dataSource;
 	}
 
-//	// Initialisation de postgres BDD
+	// Initialisation de postgres BDD
 //	@Bean
 //	public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
 //		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -29,3 +29,7 @@ VALUES ('ENEDIS', 'ENDIS@email.fr', '2023-01-01 12:00:00', NULL, 3);
 -- Subcontractor 5
 INSERT INTO subcontractor (s_name, s_email, s_fk_status_id)
 VALUES ('LYON', 'LYON@email.fr', 2);
+
+-- Prestataire 1
+INSERT INTO service_provider (sp_first_name, sp_name, sp_email, sp_creation_date, sp_lastUpdate_date,sp_fk_subcontractor_id, sp_fk_status_id)
+VALUES ('sp1', 'sp1_name' ,'ENDIS@email.fr', '2023-01-01 12:00:00', NULL,1 , 3);
