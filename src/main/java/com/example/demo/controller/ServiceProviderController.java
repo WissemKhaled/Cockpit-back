c2f2910340abd02333515b45f12d8862834c77ee
MaalainineB <bouchalga60@gmail.com>
2023-11-22 17:22:46 +0100

    unfinished word

--- src/main/java/com/example/demo/controller/ServiceProviderController.java
+++ src/main/java/com/example/demo/controller/ServiceProviderController.java
@@ -1,40 +1,61 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.ServiceProvider;
+import com.example.demo.exception.ServiceProviderNotFoundException;
+import com.example.demo.mappers.ServiceProviderDtoMapper;
+import com.example.demo.service.JwtServiceImplementation;
+import com.example.demo.service.ServiceProviderService;
+
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
-@Controller
 @AllArgsConstructor
+@RestController
+@RequestMapping("/service-providers")
 public class ServiceProviderController {
-//	
-//	private final ServiceProviderService serviceProviderService;
-//	
-//	@Autowired
-//	@Qualifier("userDetailsService")
-//	private UserDetailsService userDetailsService;
-//
-//	@Autowired
-//	private JwtServiceImplementation jwtService;
-//	
-//	public ResponseEntity<ServiceProviderDto> getServiceProviderById(int spId, HttpServletRequest request) {
-////		String authorizationHeader = request.getHeader("Authorization");
-////		try {
-////			 if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-////				 String token = authorizationHeader.substring(7);
-////				 String email = jwtService.extractUsername(token);
-////				 UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-////				 if (jwtService.validateToken(token, userDetails)) {
-////		            	return new ResponseEntity<>(serviceProviderService.)
-////		            } else {
-////		                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-////		            }
-////			 } else {
-////				 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-////			 }
-////		} catch (Exception e) {
-////			// TODO: handle exception
-////		}
-//		return new ResponseEntity<ServiceProviderDto>(HttpStatus.OK);
-//	}
+
+	private final ServiceProviderService serviceProviderService;
+	private final ServiceProviderDtoMapper providerDtoMapper;
+
+	@Autowired
+	@Qualifier("userDetailsService")
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtServiceImplementation jwtService;
+
+	@GetMapping("/{spId}")
+	public ResponseEntity<ServiceProvider> getServiceProviderById(int spId, HttpServletRequest request) {
+		String authorizationHeader = request.getHeader("Authorization");
+		try {
+			if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+				String token = authorizationHeader.substring(7);
+				String email = jwtService.extractUsername(token);
+				UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+				Boolean isTokenValid = jwtService.validateToken(token, userDetails);
+				if (isTokenValid) {
+					serviceProviderService.getServiceProviderById(spId).stream();
+					return new ResponseEntity<>(serviceProviderService.getServiceProviderById(spId), HttpStatus.OK);
+				} else {
+					return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+				}
+			} else {
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			}
+		} catch (ServiceProviderNotFoundException e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
 }
--- src/test/java/com/example/demo/controller/ServiceProviderControllerTest.java
+++ src/test/java/com/example/demo/controller/ServiceProviderControllerTest.java
@@ -1,36 +1,49 @@
 package com.example.demo.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.example.demo.service.ServiceProviderService;
 
 @SpringBootTest
 public class ServiceProviderControllerTest {
-//	
-//	private String baseUrl = "/service-provider/";
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//	
-//	@Mock
-//	private ServiceProviderService serProviderService;
-//	
-//	
-//	@Test
-//	public void testGetServiceProvider_ShouldReturnHttpStatusOk() throws Exception {
-//		int expectedSpId = 1;
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-//		LocalDateTime expectedSCreationDate = LocalDateTime.parse("2023-01-01 12:00:00", formatter);
-//		DateTimeFormatter responseFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-//
-//		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + expectedSpId).contentType(MediaType.APPLICATION_JSON))
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.spId").value(expectedSpId))
-//				.andExpect(jsonPath("$.spFirstName").value("sp-1-name"))
-//				.andExpect(jsonPath("$.spName").value("sp-1-first-name"))
-//				.andExpect(jsonPath("$.spEmail").value("sp-1-email"))
-//				.andExpect(jsonPath("$.spCreationDate").value(expectedSCreationDate.format(responseFormatter)))
-//				.andExpect(jsonPath("$.spLastUpdateDate").isEmpty())
-//				.andExpect(jsonPath("$.subcontractor.spId").value(1))
-//				.andExpect(jsonPath("$.status.stId").value(3))
-//				.andExpect(jsonPath("$.status.stName").value("En cours"));
-//		}
+	
+	private String baseUrl = "/service-providers/";
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Mock
+	private ServiceProviderService serProviderService;
+	
+	
+	@Test
+	public void testGetServiceProvider_ShouldReturnHttpStatusOk() throws Exception {
+		int expectedSpId = 1;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime expectedSCreationDate = LocalDateTime.parse("2023-01-01 12:00:00", formatter);
+		DateTimeFormatter responseFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + expectedSpId).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.spId").value(expectedSpId))
+				.andExpect(jsonPath("$.spFirstName").value("sp-1-name"))
+				.andExpect(jsonPath("$.spName").value("sp-1-first-name"))
+				.andExpect(jsonPath("$.spEmail").value("sp-1-email"))
+				.andExpect(jsonPath("$.spCreationDate").value(expectedSCreationDate.format(responseFormatter)))
+				.andExpect(jsonPath("$.spLastUpdateDate").isEmpty())
+				.andExpect(jsonPath("$.subcontractor.spId").value(1))
+				.andExpect(jsonPath("$.status.stId").value(3));
+		}
 }
