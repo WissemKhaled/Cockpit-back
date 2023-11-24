3489a573ba58b13597b969130bbd2f56884ee009
MaalainineB <bouchalga60@gmail.com>
2023-11-24 15:22:23 +0100

    refactor code in spControllertest

--- src/test/java/com/example/demo/controller/ServiceProviderControllerTest.java
+++ src/test/java/com/example/demo/controller/ServiceProviderControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -9,7 +10,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,7 +23,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.ServiceProviderDto;
-import com.example.demo.exception.ServiceProviderNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Jwts;
@@ -28,11 +30,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
-import static org.hamcrest.Matchers.hasSize;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // pour utiliser @BeforeAll et @AfterAll dans les méthodes non
+												// statiques,on doit ajouter à notre test
+												// @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+												// pour éviter la lancement de JUnitException
 public class ServiceProviderControllerTest {
 
 	@Value("${jwt.secret}")
@@ -42,41 +46,40 @@ public class ServiceProviderControllerTest {
 	private int tokenExpirationDuration;
 
 	private String baseUrl = "/service-providers/";
+	private String jwtToken;
 
 	@Autowired
 	private MockMvc mockMvc;
 
+	@BeforeAll
+	private void setUpBeforeClass() {
+		jwtToken = createToken("john@test.fr");
+	}
+
 	@Test
 	public void testGetServiceProvider_ShouldReturnHttpStatusOk() throws Exception {
 		int expectedSpId = 1;
-		String jwtToken = createToken("john@test.fr");
 
-		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + expectedSpId)
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.spFirstName").value("sp-1-first_name"))
-		.andExpect(jsonPath("$.spName").value("sp-1-name"))
-		.andExpect(jsonPath("$.spEmail").value("sp1@email.com"))
-		.andExpect(jsonPath("$.subcontractorId").value(1))
-		.andExpect(jsonPath("$.spStatusId").value(1));
+		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + expectedSpId).header("Authorization", "Bearer " + jwtToken)
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.spFirstName").value("sp-1-first_name"))
+				.andExpect(jsonPath("$.spName").value("sp-1-name"))
+				.andExpect(jsonPath("$.spEmail").value("sp1@email.com"))
+				.andExpect(jsonPath("$.subcontractorId").value(1)).andExpect(jsonPath("$.spStatusId").value(1));
 	}
 
 	@Test
 	public void testGetServiceProvider_ShouldReturnHttpStatusNotFound() throws Exception {
 		int nonExistingServiceProviderId = Integer.MAX_VALUE - 16;
-		String jwtToken = createToken("john@test.fr");
 
 		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + nonExistingServiceProviderId)
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNotFound());
+				.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
 	}
 
 	@Test
 	public void testInsertServiceProvider_ShouldReturnHttpStatusCreated() throws Exception {
 		int expectedSpId = Integer.MAX_VALUE - 5456;
-		String jwtToken = createToken("john@test.fr");
 
 		ServiceProviderDto nonExistingServiceProviderTosave = new ServiceProviderDto();
 		nonExistingServiceProviderTosave.setSpId(5);
@@ -86,21 +89,17 @@ public class ServiceProviderControllerTest {
 		nonExistingServiceProviderTosave.setSubcontractorId(1);
 		nonExistingServiceProviderTosave.setSpStatutId(1);
 
-		mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "save")
-				.content(asJsonString(nonExistingServiceProviderTosave))
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isCreated())
-		.andExpect(jsonPath("$.spFirstName").value("sp-5-first_name"))
-		.andExpect(jsonPath("$.spName").value("sp-5-name"))
-		.andExpect(jsonPath("$.spEmail").value("sp5@email.com"))
-		.andExpect(jsonPath("$.subcontractorId").value(1))
-		.andExpect(jsonPath("$.spStatusId").value(1));
+		mockMvc.perform(
+				MockMvcRequestBuilders.post(baseUrl + "save").content(asJsonString(nonExistingServiceProviderTosave))
+						.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated()).andExpect(jsonPath("$.spFirstName").value("sp-5-first_name"))
+				.andExpect(jsonPath("$.spName").value("sp-5-name"))
+				.andExpect(jsonPath("$.spEmail").value("sp5@email.com"))
+				.andExpect(jsonPath("$.subcontractorId").value(1)).andExpect(jsonPath("$.spStatusId").value(1));
 	}
 
 	@Test
 	public void testUpdateServiceProvider_UpdateExistingServiceProvider_ShouldReturnHttpOk() throws Exception {
-		String jwtToken = createToken("john@test.fr");
 
 		ServiceProviderDto existingServiceProviderTosave = new ServiceProviderDto();
 		existingServiceProviderTosave.setSpId(1);
@@ -118,73 +117,63 @@ public class ServiceProviderControllerTest {
 		ServiceProviderForUpdate.setSubcontractorId(2);
 		ServiceProviderForUpdate.setSpStatutId(1);
 
-		mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "save")
-				.content(asJsonString(ServiceProviderForUpdate))
+		mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "save").content(asJsonString(ServiceProviderForUpdate))
 				.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.spFirstName").value("sp-1-first_name_UPDATED"))
-		.andExpect(jsonPath("$.spName").value("sp-1-name_UPDATED"))
-		.andExpect(jsonPath("$.spEmail").value("sp1@email-UPDATED.com"))
-		.andExpect(jsonPath("$.subcontractorId").value(2))
-		.andExpect(jsonPath("$.spStatusId").value(1));
+				.andExpect(status().isOk()).andExpect(jsonPath("$.spFirstName").value("sp-1-first_name_UPDATED"))
+				.andExpect(jsonPath("$.spName").value("sp-1-name_UPDATED"))
+				.andExpect(jsonPath("$.spEmail").value("sp1@email-UPDATED.com"))
+				.andExpect(jsonPath("$.subcontractorId").value(2)).andExpect(jsonPath("$.spStatusId").value(1));
 	}
 
 	@Test
-	public void archiveTest_ArchiveExistingServiceProvider_ShouldReturnOk() throws Exception{
-		String jwtToken = createToken("john@test.fr");
+	public void archiveTest_ArchiveExistingServiceProvider_ShouldReturnOk() throws Exception {
 		int existingNonArchivedServiceProviderId = 1;
 		mockMvc.perform(MockMvcRequestBuilders.put(baseUrl + "archive/" + existingNonArchivedServiceProviderId)
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk());
+				.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
 	}
 
 	@Test
 	public void archiveTest_ArchiveExistingServiceProviderFailed_ShouldReturnAlreadyArchivedException()
 			throws Exception {
-		String jwtToken = createToken("john@test.fr");
 		int existingArchivedServiceProviderId = 4;
 
 		mockMvc.perform(MockMvcRequestBuilders.put(baseUrl + "archive/" + existingArchivedServiceProviderId)
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isBadRequest())
-		.andExpect(content().string("le prestataire avec l'id "+ existingArchivedServiceProviderId +" a été déjà archivé."));
+				.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest()).andExpect(content().string(
+						"le prestataire avec l'id " + existingArchivedServiceProviderId + " a été déjà archivé."));
 	}
-	
+
 	@Test
 	public void getTest_GetServiceProvidersBySubcontractorId_ShouldReturnListOfTwoServiceProviders() throws Exception {
-		String jwtToken = createToken("john@test.fr");
 		int existingSubcontractorId = 1;
-		
-		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl+"all-service-providers/"+existingSubcontractorId)
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$").isArray())
-        .andExpect(jsonPath("$", hasSize(2)))
-        .andExpect(jsonPath("$[0].spName").value("sp-1-name"))
-        .andExpect(jsonPath("$[0].spFirstName").value("sp-1-first_name"))
-        .andExpect(jsonPath("$[1].spName").value("sp-2-name"))
-        .andExpect(jsonPath("$[1].spFirstName").value("sp-2-first_name"));
+
+		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "all-service-providers/" + existingSubcontractorId)
+				.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(jsonPath("$").isArray()).andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$[0].spName").value("sp-1-name"))
+				.andExpect(jsonPath("$[0].spFirstName").value("sp-1-first_name"))
+				.andExpect(jsonPath("$[1].spName").value("sp-2-name"))
+				.andExpect(jsonPath("$[1].spFirstName").value("sp-2-first_name"));
 
 	}
-	
+
 	@Test
-	public void getTest_GetServiceProvidersBySubcontractorIdFailed_ShouldReturnServiceProvidersNotFoundException() throws Exception {
-		String jwtToken = createToken("john@test.fr");
+	public void getTest_GetServiceProvidersBySubcontractorIdFailed_ShouldReturnServiceProvidersNotFoundException()
+			throws Exception {
 		int SubcontractorIdHasNoServiceProviders = 5;
-		
-		mockMvc.perform(MockMvcRequestBuilders.get(baseUrl+"all-service-providers/"+SubcontractorIdHasNoServiceProviders)
-				.header("Authorization", "Bearer " + jwtToken)
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNotFound())
-        .andExpect(content().string(String.format("Le sous-traitant avec l'id: %d n'a pas de prestataires", SubcontractorIdHasNoServiceProviders)));
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.get(baseUrl + "all-service-providers/" + SubcontractorIdHasNoServiceProviders)
+						.header("Authorization", "Bearer " + jwtToken).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andExpect(content().string(String.format("Le sous-traitant avec l'id: %d n'a pas de prestataires",
+						SubcontractorIdHasNoServiceProviders)));
 	}
 
 	// method pour convertir un objet Java en sa représentation JSON sous forme de
 	// chaîne de caractères.
-	public static String asJsonString(Object object) {
+	private String asJsonString(Object object) {
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
 			return objectMapper.writeValueAsString(object);
