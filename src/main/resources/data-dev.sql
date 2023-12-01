b55807a8641501892e69a0d079c3d9449ad33599
yb74 <yb74@hotmail.fr>
2023-12-01 14:40:31 +0100

    removing useless script

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -14,10 +14,6 @@ VALUES (3, 'Validé', NULL);
 INSERT INTO status (st_id, st_name, st_description)
 VALUES (4, 'Archivé', NULL);
 
--- gst_message_model
-INSERT INTO gst_message_model (mm_type, mm_subject, mm_body, mm_fk_status_id)
-VALUES ('En_cour', 'Recueil Info Admin | Référencement fournisseur sous-traitant', 'je suis le body du mail',1);
-
 -- Subcontractor 1
 INSERT INTO subcontractor (s_name, s_email, s_fk_status_id)
 VALUES ('Orange', 'Orange@email.fr', 1);
