6571a32c2ba0af9bdac28bacdef299fd9d28df5e
Hamza HAMZAOUI <HHA24085@open-groupe.com>
2023-12-13 11:25:51 +0100

    ajout signature mail

--- src/main/java/com/example/demo/service/SendMailService.java
+++ src/main/java/com/example/demo/service/SendMailService.java
@@ -1,18 +1,23 @@
 package com.example.demo.service;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.dto.SendMailDTO;
 import com.example.demo.entity.SendMail;
 
+import io.jsonwebtoken.io.IOException;
 import jakarta.mail.MessagingException;
 
 public interface SendMailService {
 	
-	SendMail saveAndSendMail (String to, String subject, String body, String sender,  List<MultipartFile> file)  throws  MessagingException;
+	SendMail saveAndSendMail (String to, String subject, String body, String sender,  List<MultipartFile> file)  throws  MessagingException ;
 
+	 
 }
+
--- src/main/java/com/example/demo/service/SendMailServiceImpl.java
+++ src/main/java/com/example/demo/service/SendMailServiceImpl.java
@@ -1,14 +1,23 @@
 package com.example.demo.service;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.SendMail;
@@ -26,38 +35,42 @@ public class SendMailServiceImpl implements SendMailService {
 
 	private final JavaMailSender mailSender;
 
+	// private final ResourceLoader resourceLoader;
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
 	@Override
-	public SendMail saveAndSendMail(String to, String subject, String body, String sender,  List<MultipartFile> files) throws  MessagingException {
-		
-		System.err.println("err");
+	public SendMail saveAndSendMail(String to, String subject, String body, String sender, List<MultipartFile> files)
+			throws MessagingException {
 
 		MimeMessage message = getMimeMessage();
 		try {
-			
-			
+
 			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 			
-			
+			String signature = loadSignature();
+			signature = signature.replace("[[nom]]", "et ouiassss mumu");
+			System.err.println(signature);
+
 			helper.setPriority(1);
 			helper.setFrom("jesuisuneAdressMail@test.fr");
 			helper.setTo(to);
 			helper.setSubject(subject);
-			helper.setText(body, true);
-			//helper.setReplyTo(to);
-			
-			if(files != null && !files.isEmpty()) {
-				
+			helper.setText(body + "<br><br>" +signature, true);
+			// helper.setReplyTo(to);
+
+			if (files != null && !files.isEmpty()) {
+
 				for (MultipartFile file : files) {
 					Resource resource = new ByteArrayResource(file.getBytes());
 					helper.addAttachment(file.getOriginalFilename(), resource);
 				}
 			}
+
 			
 			mailSender.send(message);
-			
-			
-			
-			
+
 		} catch (MailException | IOException e) {
 
 			System.err.println(e.getMessage());
@@ -65,10 +78,19 @@ public class SendMailServiceImpl implements SendMailService {
 
 		return null;
 	}
-	
-	
+
 	private MimeMessage getMimeMessage() {
 		return mailSender.createMimeMessage();
+	}
+
+	public String loadSignature() throws IOException {
+		String signaturePath = "signature.html";
+
+		Resource resource = new ClassPathResource(signaturePath);
+
+		try (InputStreamReader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+			return FileCopyUtils.copyToString(reader);
+		}
 	}
 
 }
