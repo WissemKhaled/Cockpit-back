5d4c597a8ced6184028951c29157ae5266b7d269
MaalainineB <bouchalga60@gmail.com>
2023-12-15 00:19:36 +0100

    GstLogBuilder added

---
+++ src/main/java/com/example/demo/entity/builder/GstLogBuilder.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity.builder;
+
+import java.time.LocalDateTime;
+
+import com.example.demo.entity.GstLog;
+
+public class GstLogBuilder {
+	private int logId;
+	private String logType;
+	private String logEmail;
+	private String logValue;
+	private LocalDateTime logCreationDate;
+
+	public GstLogBuilder setLogId(int logId) {
+		this.logId = logId;
+		return this;
+	}
+
+	public GstLogBuilder setLogType(String logType) {
+		this.logType = logType;
+		return this;
+	}
+
+	public GstLogBuilder setLogEmail(String logEmail) {
+		this.logEmail = logEmail;
+		return this;
+	}
+
+	public GstLogBuilder setLogValue(String logValue) {
+		this.logValue = logValue;
+		return this;
+	}
+
+	public GstLogBuilder setLogCreationDate(LocalDateTime logCreationDate) {
+		this.logCreationDate = logCreationDate;
+		return this;
+	}
+
+	public GstLog build() {
+		return new GstLog(logId, logType, logEmail, logValue, logCreationDate);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/builder/MessageModelBuilder.java
@@ -0,0 +1,56 @@
+package com.example.demo.entity.builder;
+
+import java.time.LocalDateTime;
+
+import com.example.demo.entity.MessageModel;
+import com.example.demo.entity.Status;
+
+public class MessageModelBuilder {
+	private Integer mmId;
+	private String mmType;
+	private String mmSubject;
+	private String mmBody;
+	private LocalDateTime mmCreationDate;
+	private LocalDateTime mmLastUpdateDate;
+	private Status status;
+
+	public MessageModelBuilder withMmId(Integer mmId) {
+		this.mmId = mmId;
+		return this;
+	}
+
+	public MessageModelBuilder withMmType(String mmType) {
+		this.mmType = mmType;
+		return this;
+	}
+
+	public MessageModelBuilder withMmSubject(String mmSubject) {
+		this.mmSubject = mmSubject;
+		return this;
+	}
+
+	public MessageModelBuilder withMmBody(String mmBody) {
+		this.mmBody = mmBody;
+		return this;
+	}
+
+	public MessageModelBuilder withMmCreationDate(LocalDateTime mmCreationDate) {
+		this.mmCreationDate = mmCreationDate;
+		return this;
+	}
+
+	public MessageModelBuilder withMmLastUpdateDate(LocalDateTime mmLastUpdateDate) {
+		this.mmLastUpdateDate = mmLastUpdateDate;
+		return this;
+	}
+
+	public MessageModelBuilder withStatus(Status status) {
+		this.status = status;
+		return this;
+	}
+
+	public MessageModel build() {
+		return new MessageModel(mmId, mmType, mmSubject, mmBody, mmCreationDate, mmLastUpdateDate, status);
+	}
+
+}
