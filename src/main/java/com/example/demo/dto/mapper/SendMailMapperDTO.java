099b154603106ebdb4a2f39107a9869b9e8a8fd4
hamza <66466122+hamza93290@users.noreply.github.com>
2024-01-05 11:43:21 +0100

    refactor messageSend

--- src/main/java/com/example/demo/dto/mapper/SendMailMapperDTO.java
+++ src/main/java/com/example/demo/dto/mapper/SendMailMapperDTO.java
@@ -3,18 +3,22 @@ package com.example.demo.dto.mapper;
 import com.example.demo.dto.SendMailDTO;
 import com.example.demo.entity.SendMail;
 import com.example.demo.entity.UUser;
+import com.example.demo.service.UserInfoService;
 
 public class SendMailMapperDTO {
+	
+	private UserInfoService infoService;
 
 	public SendMailDTO toDto(SendMail sendMail) {
-		return new SendMailDTO(sendMail.getMsId(), sendMail.getMsSender(), sendMail.getMsTo(), sendMail.getMsCc(),sendMail.getMsSubject(),
-				sendMail.getMsBody(), sendMail.getMsError(), sendMail.getMsStatus(), sendMail.getMsCreationsDate());
+		return new SendMailDTO(sendMail.getMsId(), sendMail.getMsTo(), sendMail.getMsCc(),sendMail.getMsSubject(),
+				sendMail.getMsBody(), sendMail.getMsSendDate() , sendMail.getUser());
 
 	}
 
 	public SendMail toSendMail(SendMailDTO dto) {
-		return new SendMail(dto.getMsId(), dto.getMsSender(), dto.getMsTo(), dto.getMsCc(),dto.getMsSubject(),
-				dto.getMsBody(), dto.getMsError(), dto.getMsStatus(), dto.getMsCreationsDate());
+		return new SendMail(dto.getMsId(), dto.getMsTo(), dto.getMsCc(),dto.getMsSubject(),
+				dto.getMsBody(), dto.getMsSendDate() , dto.getUser());
+
 	}
 
 }
