46dfb26d5e26a4af8ea9b78f1adaac4942d4ba9a
MaalainineB <bouchalga60@gmail.com>
2024-01-06 20:06:31 +0100

    refactor code

--- src/main/java/com/example/demo/mappers/StatusMapper.java
+++ src/main/java/com/example/demo/mappers/StatusMapper.java
@@ -3,21 +3,14 @@ package com.example.demo.mappers;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.entity.Status;
 
 @Mapper
 public interface StatusMapper {
-	@Select("SELECT * FROM gst_status WHERE st_id = #{stId}")
-	@Result(property = "stId", column = "st_id")
-	@Result(property = "stName", column = "st_name")
-	Status findStatusById(int stId);
+
+	Status findStatusById(@Param("stId") int stId);
 	
-	// debut cette method recupere tous les status
-//	@Select("SELECT * FROM gst_status ORDER BY st_id")
-//	@Result(property = "stId", column = "st_id")
-//	@Result(property = "stName", column = "st_name")
 	List<Status> getAllStatus();
 }
