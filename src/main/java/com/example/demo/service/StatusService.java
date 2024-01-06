13ac9a92d373ee7676937ad7e9f49cd133f5d3ce
MaalainineB <bouchalga60@gmail.com>
2024-01-06 19:47:29 +0100

    statusApi added

--- src/main/java/com/example/demo/service/StatusService.java
+++ src/main/java/com/example/demo/service/StatusService.java
@@ -1,7 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.dto.StatusDto;
 import com.example.demo.entity.Status;
 
 public interface StatusService {
 	Status getStatusById(int stId);
+
+	List<StatusDto> getAllStatus();
 }
