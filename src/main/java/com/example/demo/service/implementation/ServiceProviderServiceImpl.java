de107ae8a7d234b363af2c98115e2c12530a1163
MaalainineB <bouchalga60@gmail.com>
2024-01-09 12:30:04 +0100

    merge conflicts resolved

--- src/main/java/com/example/demo/service/implementation/ServiceProviderServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/ServiceProviderServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
